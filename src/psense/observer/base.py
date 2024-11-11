f13f25e4557ef65e344582a44724b757bab4434a
Vladislav A. Proskurov <rilshok@pm.me>
2024-11-11 20:52:41 +0000

    feat: in-process measurement

--- src/psense/observer/base.py
+++ src/psense/observer/base.py
@@ -1,20 +1,31 @@
+import time
 from abc import ABC, abstractmethod
 from typing import Generic, TypeVar
 from typing_extensions import Self
 from types import TracebackType
+from threading import Thread
 
 T = TypeVar("T")
 
 
 class Observer(ABC, Generic[T]):
-    def __init__(self) -> None:
+    def __init__(self, interval: float | None = None) -> None:
         self._before: list[T] = []
+        self._during: list[list[T]] = []
         self._after: list[T] = []
+        self._interval = interval
+        self._running = False
+        self._thread: Thread | None = None
 
     @property
     def before(self) -> list[T]:
         """The list of assays taken before entering the context"""
         return self._before.copy()
+
+    @property
+    def during(self) -> list[list[T]]:
+        """The list of assays taken during the context"""
+        return [d.copy() for d in self._during]
 
     @property
     def after(self) -> list[T]:
@@ -26,9 +37,20 @@ class Observer(ABC, Generic[T]):
         """Take an assay of the current state"""
         raise NotImplementedError()
 
+    def _run(self, interval: float) -> None:
+        while self._running:
+            time.sleep(interval / 2)
+            self._during[-1].append(self.assay())
+            time.sleep(interval / 2)
+
     def __enter__(self) -> Self:
-        """Take an initial assay when entering the context manager"""
+        """Take an initial assay and start monitoring if interval is set"""
         self._before.append(self.assay())
+        self._during.append([])
+        if self._interval is not None:
+            self._running = True
+            self._thread = Thread(target=self._run, args=(self._interval,))
+            self._thread.start()
         return self
 
     def __exit__(
@@ -37,6 +59,9 @@ class Observer(ABC, Generic[T]):
         value: BaseException | None,  # noqa: F841
         traceback: TracebackType | None,  # noqa: F841
     ) -> None:
-        """Take a final assay when exiting the context manager"""
-        end_value = self.assay()
-        self._after.append(end_value)
+        """Stop monitoring and take a final assay when exiting the context manager"""
+        if self._interval is not None:
+            self._running = False
+            if self._thread:
+                self._thread.join()
+        self._after.append(self.assay())
