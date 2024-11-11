8295e1649e12439062925ff1931f6097804f7c69
Vladislav A. Proskurov <rilshok@pm.me>
2024-11-11 20:33:47 +0000

    feat: Process cpu usage

--- src/psense/observer/cpu.py
+++ src/psense/observer/cpu.py
@@ -30,3 +30,30 @@ class CPUUsageObserver(Observer[CPUUsage]):
             system=cpu_times.system,
             idle=cpu_times.idle,
         )
+
+
+@attr.s(slots=True, kw_only=True)
+class ProcessCPUUsage:
+    # Percentage of CPU used by the process
+    percent: float = attr.ib()
+
+    # System-wide CPU times used by the process
+    user: float = attr.ib()
+    system: float = attr.ib()
+
+
+class ProcessCPUUsageObserver(Observer[ProcessCPUUsage]):
+    def __init__(self, interval: float | None = None, pid: int | None = None):
+        super().__init__()
+        self._interval = interval
+        self._process = psutil.Process(pid)
+
+    def assay(self) -> ProcessCPUUsage:
+        cpu_percent = self._process.cpu_percent(interval=self._interval)
+        cpu_times = self._process.cpu_times()
+
+        return ProcessCPUUsage(
+            percent=cpu_percent,
+            user=cpu_times.user,
+            system=cpu_times.system,
+        )
